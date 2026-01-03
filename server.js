80ecd4b92eb3489ab252369657060b2cf9442eb6
myeiodigitalsolutions-sys <myeiodigitalsolutions@gmail.com>
2026-01-03 10:39:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/cart', require('./routes/cartRoutes'));
 app.use('/api/contact', contactRoutes);
 
+
 // MongoDB Connection
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/threeDModuleToys';
 
